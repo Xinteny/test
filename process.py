8a38bf115a844927c5218520f64fa6c2ebecccf0
Cover <xinteny@163.com>
2022-07-21 18:30:10 +0800

    first commit

--- process.py
+++ process.py
@@ -6,14 +6,15 @@ def un_gz(file_name):
     #获取文件的名称，去掉
     g_file = gzip.GzipFile(file_name)
     #创建gzip对象
-    open(f_name, "wb+").write(g_file.read())
+    outpath = os.path.join(os.getenv('OUTPUT_PATH'), f_name)
+    open(outpath, "wb+").write(g_file.read())
     #gzip对象用read()打开后，写入open()建立的文件里。
     g_file.close()
     #关闭gzip对象
 path = os.getenv('DATA_PATH')
 name = os.listdir(path)
 filename = os.path.join(path, name)
-un_gz('filename')
+un_gz(filename)
 # print(os.getenv('DATA_PATH'))
 # print(os.path.join(os.getenv('DATA_PATH'), '自定义下载文件名'))
 # os.path.join(os.getenv('OUTPUT_PATH'), '生成文件名')
