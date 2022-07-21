d507ec1c9d02d08fd0991c9d9dd6bf95419f4845
Cover <xinteny@163.com>
2022-07-21 16:25:33 +0800

    first commit

--- process.py
+++ process.py
@@ -1,3 +1,4 @@
+import os
 import gzip
 def un_gz(file_name):
     """ungz zip file"""
@@ -9,4 +10,6 @@ def un_gz(file_name):
     #gzip对象用read()打开后，写入open()建立的文件里。
     g_file.close()
     #关闭gzip对象
-un_gz('Landsat8_1001.tif.gz')
+# un_gz('Landsat8_1001.tif.gz')
+print(os.path.join(os.getenv('DATA_PATH'), '自定义下载文件名'))
+os.path.join(os.getenv('OUTPUT_PATH'), '生成文件名')
